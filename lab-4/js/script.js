1f8926eba6f0d542ec567bffbb38aff8de176ebc
Manjinder Singh <mansingh41100@gmail.com>
2026-04-03 18:12:28 -0400

    JS to change bg color of sorted col

--- lab-4/js/script.js
+++ lab-4/js/script.js
@@ -1,2 +1,37 @@
 const table = document.querySelector("table");
 let tableSort = new Tablesort(table);
+
+function sortColBgColorChange(clickedCol){
+    const tableBodyRows = document.querySelectorAll("tbody tr");
+
+    const tableHeaders = document.querySelectorAll("thead th");
+
+    tableHeaders.forEach((th, index) => {
+        th.style.backgroundColor = "#fff";
+        if (index == clickedCol){
+            th.style.backgroundColor = "#cceeff";
+        }
+    });
+
+    console.log(tableBodyRows)
+
+    tableBodyRows.forEach( (row, index) =>{
+        for (let i = 0; i < row.children.length; i++) {
+            row.children[i].style.backgroundColor = "#fff";
+            if(i == clickedCol){
+                row.children[i].style.backgroundColor = "#cceeff";
+            }
+        }
+    })
+
+}
+
+const tableHeaders = document.querySelectorAll("thead th");
+
+
+tableHeaders.forEach((element, index) => {
+    element.addEventListener('click', event =>{
+        sortColBgColorChange(index)
+    })
+});
+
